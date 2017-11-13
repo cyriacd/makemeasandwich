e1e6cb397e7a23c8f6328569e50325b6332f31f9
Cyriac Domini <me@cyriacdomini.com>
2017-11-13 11:08:42 +0000

    :no_entry_sign: removed unused impl files

--- nodejsimpl/lib/makemeasandwich.js
+++
@@ -1,9 +0,0 @@
-// Require sudo on unix platforms
-if(process.platform !== 'win32'){
-  if(process.env.USER !== "root"){
-    console.log("What? Make it yourself.");
-    process.exit();
-  }else{
-    console.log("Sandwich made. Go get it!");
-  }
-}
